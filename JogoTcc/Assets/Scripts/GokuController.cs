c6bd32dc001448a4039babeb282c0b5f2e911428
JoeMyFriend <joao.paulo.miquilini@gmail.com>
2016-08-02 16:48:36 -0300

    sadsa

---
+++ JogoTcc/Assets/Scripts/GokuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GokuController : MonoBehaviour {
+
+	public Animator anime;
+
+	public Rigidbody2D rigidbody;
+
+	public bool transform;
+
+	//AUDIO
+
+	public AudioSource som;
+	public AudioClip Billl;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			transform = true;
+			Debug.Log ("CHEGOU NESSA PARTE");
+			som.PlayOneShot (Billl);
+		}
+		if(Input.GetButtonUp("Fire1")){
+			transform = false;
+		}
+
+		anime.SetBool ("transform", transform);
+	}
+}
---
+++ JogoTcc/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public Animator anime;
+	public Rigidbody2D 	playerRigidBody;
+	public int forceJump;
+	public int speed;
+	public int maxSpeed;
+
+	public bool jump;
+	public bool run;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Jump")) {
+			playerRigidBody.AddForce (new Vector2 (0, forceJump));
+			jump = true;
+
+		}
+		if(Input.GetAxisRaw("Horizontal") > 0){
+			playerRigidBody.AddForce (new Vector2((speed) * Time.deltaTime, 0));
+			run = true;
+			playerRigidBody.transform.eulerAngles = new Vector2 (0, 0);
+		}
+
+		if(Input.GetAxisRaw("Horizontal") < 0){
+			playerRigidBody.AddForce (new Vector2((speed) * Time.deltaTime * -1, 0));
+			run= true;
+			playerRigidBody.transform.eulerAngles = new Vector2 (0, 180);
+		}
+
+		if(Input.GetButtonUp("Horizontal")){
+			run = false;
+		}
+
+		anime.SetBool ("run", run);
+
+	}
+}
